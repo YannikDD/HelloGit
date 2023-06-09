1a06daa001a56226171dd60e3c27e6a47b4cdace
s1317004 <s1317004@u-aizu.ac.jp>
2023-06-09 12:13:39 +0900

    Dice Game: initial revision

---
+++ DiceGame.py
@@ -0,0 +1,17 @@
+#Author s1317004
+
+import random
+
+def roll_dice(): 
+    print("Rolling dice...")
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
+    print("Die 1:", die1)
+    print("Die 2:", die2)
+    print("Total value:", die1 + die2)
+
+
+
+roll_dice()
+
+input("press enter to exit...")
